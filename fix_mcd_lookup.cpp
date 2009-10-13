ddedb9b168dc4a0b57cabc618fdc4edfa00bab9d
Matthew Hooker <mhooker@mhookerosx.cnet.cnwk>
2009-10-12 21:31:28 -0700

    removes comments and debugging code

--- fix_mcd_lookup.cpp
+++ fix_mcd_lookup.cpp
@@ -13,34 +13,22 @@ class MCCheck : public Visitor {
 public:
     MCCheck() : from_if(false) {};
     void post_assignment(Assignment* in) {
-        /*
-        if (isa<Method_invocation>(in->expr) && 
-               dynamic_cast<Method_invocation*>(in->expr)->match(
-                  new METHOD_NAME(new String("get_cache")))) {
-           cout << "Got one!" << endl;  
-        }*/
         if (isa<Method_invocation>(in->expr) 
                 && dynamic_cast<Method_invocation*>(in->expr)
                 ->method_name->match(new METHOD_NAME(new String("get_cache")))) 
         {
             v = in->variable;
-            cout << "true" << endl; 
         }
-        //METHOD_NAME* name =dynamic_cast<METHOD_NAME*>(in->method_name);
-        //cout << name->value <<endl;
     }
     
     void pre_unary_op(Unary_op* in) {
         if (in->op->match(new OP(new String("!")))) {
-            cout << "got unary op " << in->op->value->c_str();
             if (isa<Variable>(in->expr) && from_if && in->expr->match(v)) {
                 in->expr = new Method_invocation(
                             NULL,
                             new METHOD_NAME(new String("$this->last_memcache_lookup_succeeded")),
                             new List<Actual_parameter*>()
                         );
-
-                cout << " yes";
             }
             cout << endl;
         }
